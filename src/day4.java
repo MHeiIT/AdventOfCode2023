e72b80055afeb78330849c0d29f3eee2d250f19c
Mika Benjamin Heinemann <mika.heinemann@student.tugraz.at>
2024-02-07 13:27:04 +0100

    day4 done

--- src/day4.java
+++ src/day4.java
@@ -1,28 +1,81 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class day4 {
     public static void main(String[] args) throws IOException {
 
-        List<String> file = Files.readAllLines(Path.of("./vendor/day1a"));
+        b4();
+    }
+    public static void a4() throws IOException {
+        List<String> file = Files.readAllLines(Path.of("./vendor/day4a"));
         int sum = 0;
         for (String line: file) {
-            char[] chars = line.toCharArray();
-            int first = -1;
-            int last = -1;
-            for (char c: chars) {
-                if (Character.isDigit(c)) {
-                    if (first == -1) {
-                        first = (int)(c - '0') * 10;
-                        last = (int)(c - '0');
-                    } else {
-                        last = (int)(c - '0');
+            String[] twolists = line.split(": ")[1].split(" \\| ");
+            String[] winning_numbers = twolists[0].replace("  ", " ").split(" ");
+            String[] my_numbers = twolists[1].replace("  ", " ").split(" ");
+
+            int counter = -1;
+
+            for (String winning_number: winning_numbers) {
+                for (String my_number: my_numbers) {
+                    if (my_number.equals("") || winning_number.equals("")) {
+                        continue;
+                    }
+                    if (Integer.parseInt(winning_number) == Integer.parseInt(my_number)) {
+                        counter++;
+                        break;
                     }
                 }
             }
-            sum += first + last;
+
+            if (counter != -1) {
+                sum += Math.pow(2, counter);
+            }
+        }
+        System.out.println(sum);
+    }
+    public static void b4() throws IOException {
+        List<String> file = Files.readAllLines(Path.of("./vendor/day4a"));
+
+        List<Integer> cardcount = new ArrayList<>();
+        for (String line: file) {
+            cardcount.add(1);
+        }
+
+
+        for (int i = 0; i < file.size(); i++) {
+            String line = file.get(i);
+            String[] twolists = line.split(": ")[1].split(" \\| ");
+            String[] winning_numbers = twolists[0].replace("  ", " ").split(" ");
+            String[] my_numbers = twolists[1].replace("  ", " ").split(" ");
+
+            int counter = 0;
+
+            for (String winning_number: winning_numbers) {
+                for (String my_number: my_numbers) {
+                    if (my_number.equals("") || winning_number.equals("")) {
+                        continue;
+                    }
+                    if (Integer.parseInt(winning_number) == Integer.parseInt(my_number)) {
+                        counter++;
+                        break;
+                    }
+                }
+            }
+            //System.out.println(counter);
+            if (counter != 0) {
+                for (int j = i+1; j < i + 1 + counter; j++) {
+                    cardcount.set(j, cardcount.get(j) + cardcount.get(i));
+                    //System.out.println(j);
+                }
+            }
+        }
+        int sum = 0;
+        for (int c : cardcount) {
+            sum += c;
         }
         System.out.println(sum);
     }
