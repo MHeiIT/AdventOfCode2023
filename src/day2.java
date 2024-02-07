3232a68c8f36f6ad30c2d5db2ec578c383e4fb18
Mika Benjamin Heinemann <mika.heinemann@student.tugraz.at>
2024-02-07 10:31:36 +0100

    day2 Done

--- src/day2.java
+++ src/day2.java
@@ -6,23 +6,76 @@ import java.util.List;
 public class day2 {
     public static void main(String[] args) throws IOException {
 
-        List<String> file = Files.readAllLines(Path.of("./vendor/day1a"));
+        b2();
+    }
+
+    public static void a2() throws IOException {
+        List<String> file = Files.readAllLines(Path.of("./vendor/day2a"));
         int sum = 0;
         for (String line: file) {
-            char[] chars = line.toCharArray();
-            int first = -1;
-            int last = -1;
-            for (char c: chars) {
-                if (Character.isDigit(c)) {
-                    if (first == -1) {
-                        first = (int)(c - '0') * 10;
-                        last = (int)(c - '0');
-                    } else {
-                        last = (int)(c - '0');
+            String[] split_line = line.split(":");
+            int game_number = Integer.parseInt(split_line[0].split(" ")[1]);
+            String new_line = split_line[1];
+
+            String[] cube_subsets = new_line.split(";");
+            boolean isPossible = true;
+
+            outerloop: for (String cube_subset: cube_subsets) {
+                String[] cubes = cube_subset.split(",");
+                for (String one_cube_color: cubes) {
+                    int cube_Count = Integer.parseInt(one_cube_color.substring(1).split(" ")[0]);
+                    String cube_color = one_cube_color.substring(1).split(" ")[1];
+                    if (cube_color.compareTo("red") == 0 && cube_Count > 12) {
+                        isPossible = false;
+                        break outerloop;
+                    } else if (cube_color.compareTo("blue") == 0 && cube_Count > 14) {
+                        isPossible = false;
+                        break outerloop;
+                    } else if (cube_color.compareTo("green") == 0 && cube_Count > 13) {
+                        isPossible = false;
+                        break outerloop;
                     }
                 }
             }
-            sum += first + last;
+
+            if (isPossible) {
+                sum += game_number;
+            }
+
+        }
+        System.out.println(sum);
+    }
+
+    public static void b2() throws IOException {
+        List<String> file = Files.readAllLines(Path.of("./vendor/day2a"));
+        int sum = 0;
+        for (String line: file) {
+            String[] split_line = line.split(":");
+            int game_number = Integer.parseInt(split_line[0].split(" ")[1]);
+            String new_line = split_line[1];
+
+            String[] cube_subsets = new_line.split(";");
+            int red_cube_count = 0;
+            int blue_cube_count = 0;
+            int green_cube_count = 0;
+
+            for (String cube_subset: cube_subsets) {
+                String[] cubes = cube_subset.split(",");
+                for (String one_cube_color: cubes) {
+                    int cube_Count = Integer.parseInt(one_cube_color.substring(1).split(" ")[0]);
+                    String cube_color = one_cube_color.substring(1).split(" ")[1];
+                    if (cube_color.compareTo("red") == 0 && cube_Count > red_cube_count) {
+                        red_cube_count = cube_Count;
+                    } else if (cube_color.compareTo("blue") == 0 && cube_Count > blue_cube_count) {
+                        blue_cube_count = cube_Count;
+                    } else if (cube_color.compareTo("green") == 0 && cube_Count > green_cube_count) {
+                        green_cube_count = cube_Count;
+                    }
+                }
+            }
+
+            sum += red_cube_count * blue_cube_count * green_cube_count;
+
         }
         System.out.println(sum);
     }
