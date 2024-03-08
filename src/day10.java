48a8689cfbdef135cbe63f0da648dc48ce2e36c9
Mika Heinemann <mika@hmail.at>
2024-03-08 13:19:09 +0100

    day 10 done

--- src/day10.java
+++ src/day10.java
@@ -97,7 +97,7 @@ public class day10 {
     }
 
     public static void b10() throws IOException {
-        List<String> file = Files.readAllLines(Path.of("./vendor/day10b_test"));
+        List<String> file = Files.readAllLines(Path.of("./vendor/day10a"));
         char[][] field = new char[file.size()][file.get(0).length()];
 
 
@@ -141,16 +141,125 @@ public class day10 {
             loopfield[nextposi][nextposj] = 'L';
         }
 
+
+
+
         for (int i = 0; i < loopfield.length; i++) {
             for (int j = 0; j < loopfield[i].length; j++) {
-                System.out.print(loopfield[i][j]);
+                if (loopfield[i][j] == 'L') {
+                    loopfield[i][j] = field[i][j];
+                }
+                //System.out.print(loopfield[i][j]);
+            }
+            //System.out.println();
+        }
+
+        /*System.out.println("---------------------------");
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                System.out.print(field[i][j]);
             }
             System.out.println();
         }
+        System.out.println("------------------------------------------");*/
 
+        char[][] newarr = double_size(loopfield); //double_size(double_size(loopfield));
+        /*for (int i = 0; i < newarr.length; i++) {
+            for (int j = 0; j < newarr[i].length; j++) {
+                System.out.print(newarr[i][j]);
+            }
+            System.out.println();
+        }*/
 
+        for (int i = 0; i < newarr.length; i++) {
+            if (newarr[i][0] == '.') {
+                newarr[i][0] = 'O';
+            }
+            if (newarr[i][newarr[i].length-1] == '.') {
+                newarr[i][newarr[i].length-1] = 'O';
+            }
+        }
+        for (int i = 0; i < newarr[0].length; i++) {
+            if (newarr[0][i] == '.') {
+                newarr[0][i] = 'O';
+            }
+            if (newarr[newarr.length-1][i] == '.') {
+                newarr[newarr.length-1][i] = 'O';
+            }
+        }
+
+        boolean t = true;
+
+        while (t) {
+            t = false;
+            for (int i = 0; i < newarr.length; i++) {
+                for (int j = 0; j < newarr[i].length; j++) {
+                    if (newarr[i][j] == '.') {
+                        if ((i != 0) && (newarr[i - 1][j] == 'O')) {
+                            t = true;
+                            newarr[i][j] = 'O';
+                        } else if ((i != (newarr.length - 1)) && (newarr[i + 1][j] == 'O')) {
+                            t = true;
+                            newarr[i][j] = 'O';
+                        } else if ((j != 0) && (newarr[i][j - 1] == 'O')) {
+                            t = true;
+                            newarr[i][j] = 'O';
+                        } else if ((j != (newarr[i].length - 1)) && (newarr[i][j + 1] == 'O')) {
+                            t = true;
+                            newarr[i][j] = 'O';
+                        }
+                    }
+                }
+            }
+        }
         int sum = 0;
+        for (int i = 0; i < newarr.length; i++) {
+            for (int j = 0; j < newarr[i].length; j++) {
+                if (newarr[i][j] == '.') {
+                    if (i % 2 == 0 && j % 2 == 0) {
+                        sum++;
+                    }
+                }
+            }
+        }
+
+
+
         System.out.println(sum);
     }
 
+    public static char[][] double_size(char[][] arr) {
+        char[][] field = new char[arr.length*2][arr[0].length*2];
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                field[i][j] = '.';
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                field[i*2][j*2] = arr[i][j];
+            }
+        }
+
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                if (field[i][j] == '.') {
+                    if ((i != 0) && ((field[i - 1][j] == 'F') || (field[i - 1][j] == '|') || (field[i - 1][j] == '7'))) {
+                        field[i][j] = '|';
+                    } else if ((i != (field.length - 1)) && ((field[i + 1][j] == 'J') || (field[i + 1][j] == '|') || (field[i + 1][j] == 'L'))) {
+                        field[i][j] = '|';
+                    } else if ((j != 0) && ((field[i][j - 1] == 'F') || (field[i][j - 1] == '-') || (field[i][j - 1] == 'L'))) {
+                        field[i][j] = '-';
+                    } else if ((j != (field[i].length - 1)) && ((field[i][j + 1] == 'J') || (field[i][j + 1] == '-') || (field[i][j + 1] == '7'))) {
+                        field[i][j] = '-';
+                    }
+                }
+            }
+        }
+
+
+
+        return field;
+    }
 }
--- src/day8.java
+++ src/day8.java
@@ -1,6 +1,7 @@
 import helpers.Pair;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
@@ -10,7 +11,8 @@ import static helpers.helpers.*;
 public class day8 {
     public static void main(String[] args) throws IOException {
 
-        b8();
+        //b8();
+        calc();
     }
     public static void a8() throws IOException {
         List<String> file = Files.readAllLines(Path.of("./vendor/day8a"));
@@ -56,7 +58,7 @@ public class day8 {
 
     public static void b8() throws IOException {
         List<String> file = Files.readAllLines(Path.of("./vendor/day8b"));
-        Queue<Character> queue = charArrayToQueue(file.get(0).toCharArray());
+        char[] chararr = file.get(0).toCharArray();
 
         file.remove(0);
         file.remove(0);
@@ -66,7 +68,10 @@ public class day8 {
             String newline = line.replace(")", "").replace(" = (", ",").replace(" ", "");
             String[] splitline = newline.split(",");
             String key = splitline[0];
-
+            if (mp.containsKey(key)) {
+                System.out.println("okok");
+                System.exit(-1);
+            }
             Pair<String, String> p = new Pair<>(splitline[1], splitline[2]);
             mp.put(key, p);
 
@@ -78,12 +83,17 @@ public class day8 {
         int sum = 0;
         String[] current_posses = current_posses_list.toArray(new String[0]);
 
+        for (int i = 0; i < current_posses.length; i++) {
+            System.out.println(current_posses[i] + " " + current_posses_list.get(i));
+        }
         //System.out.println(current_posses.length);
 
-        while (queue.size() != 0) { // is always true
-            char c = queue.poll();
+        int last = 0;
+        int iii = 100;
+        for (int j = 0; true; j++) {
+            char c = chararr[j % chararr.length];
 
-            sum++;
+
             boolean end = true;
 
             for (int i = 0; i < current_posses.length; i++) {
@@ -92,20 +102,76 @@ public class day8 {
                 } else {
                     current_posses[i] = mp.get(current_posses[i]).getL();
                 }
-                if (current_posses[i].toCharArray()[2] != 'Z') {
+                if (current_posses[i].toCharArray()[current_posses[i].toCharArray().length - 1] != 'Z') {
                     end = false;
+                } else {
+                    int temp = j+1;
+                    int aaa = (temp - last);
+                    System.out.println(temp + " " + aaa);
+                    last = temp;
+                    iii--;
+                    if (iii != 0) {
+                        end = false;
+                    }
                 }
-            }
-
-            if (end) {
                 break;
             }
 
-            queue.offer(c);
+            if (end) {
+                sum = j + 1;
+                break;
+            }
         }
 
         System.out.println(sum);
         // (29998-14999)*x+14999 = (40186-20093)*x+20093= (34526-17263)*x+17263= (33394-16697)*x+16697= (24338-12169)*x+12169= (41318-20659)*x+20659
         //(29998-14999)*x+14999 = (40186-20093)*x+20093= (34526-17263)*x+17263= (33394-16697)*x+16697= (41318-20659)*x+20659
+        // 14999*x+14999 = 20093*y+20093= 17263*z+17263=16697*v+16697= 20659*w+20659
+        // 14999*x = 20093*y= 17263*z=16697*v= 20659*w
+        // x mod 14999 = x mod 20093 = x mod 17263 = x mod 16697 = x mod 20659
+        // solve x mod 14999 = 0 && x mod 20093 = 0 && x mod 17263 = 0 && x mod 16697 = 0 && x mod 20659
+        // Element[x, Integers] && x > 0 && Element[y, Integers] && y > 0 && Element[z, Integers] && z > 0 && Element[v, Integers] && v > 0 && Element[w, Integers] && w > 0
+        // 14999*x+14999 = 20093*y+20093= 17263*z+17263=16697*v+16697= 20659*w+20659 && Element[x, Integers]&& Element[y, Integers]&& Element[z, Integers] && Element[v, Integers] && Element[w, Integers]
+    }
+
+    public static void calc() {
+        BigInteger[] ints = {BigInteger.valueOf(14999), BigInteger.valueOf(20093),
+                BigInteger.valueOf(17263), BigInteger.valueOf(16697),
+                BigInteger.valueOf(20659)};
+
+        BigInteger lcm = calculateLCM(ints);
+
+        BigInteger test = lcm(ints);
+
+        System.out.println("The least common multiple (LCM) is: " + lcm);
+        System.out.println("The least common multiple (LCM) is: " + test);
+    } // to high 311653045202070000
+    // to low 279785601383
+
+    public static BigInteger calculateLCM(BigInteger[] moduli) {
+        BigInteger lcm = moduli[0];
+        for (int i = 1; i < moduli.length; i++) {
+            lcm = lcm.multiply(moduli[i]).divide(lcm.gcd(moduli[i]));
+        }
+        return lcm;
+    }
+
+    public static BigInteger lcm(BigInteger... values) {
+        if (values.length == 0)
+            return BigInteger.ONE;
+        BigInteger lcm = values[0];
+        for (int i = 1; i < values.length; i++) {
+            if (values[i].signum() != 0) {
+                final BigInteger gcd = lcm.gcd(values[i]);
+                if (gcd.equals(BigInteger.ONE)) {
+                    lcm = lcm.multiply(values[i]);
+                } else {
+                    if (!values[i].equals(gcd)) {
+                        lcm = lcm.multiply(values[i].divide(gcd));
+                    }
+                }
+            }
+        }
+        return lcm;
     }
 }
